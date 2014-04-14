f127de28c90ae2cb321b3ba1e34a811b4debe240
Brandon Sleater <brandon@cerebrumcorp.com>
2014-04-14 12:29:53 -0700

    Setting up index page

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<!-- Headers -->
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="CST433 Final Project: Queries with the Selenium Apartment Database">
+        <meta name="author" content="Brandon Sleater, Joseph Stratton, Dominic">
+        <title>Selenium Apartments</title>
+
+		<!-- JQuery -->
+		<script src="https://code.jquery.com/jquery-2.1.0.min.js" defer></script>
+
+		<!-- Bootstrap -->
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="row" id="layer1">
+				<p>Test</p>
+			</div>
+			<hr>
+			<div class="row" id="layer2">
+				<p>Test2</p>
+			</div>
+		</div>
+	</body>
+</html>
