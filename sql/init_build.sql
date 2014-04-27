decb618ef71be33fe9c3fee9b6c655c8cfbc6ccf
Brandon Sleater <brandon@cerebrumcorp.com>
2014-04-27 12:23:12 -0700

    Sample data for apartment and building. Linking the two as well

--- sql/init_build.sql
+++ sql/init_build.sql
@@ -26,11 +26,18 @@ DROP TABLE IF EXISTS `apartment`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `apartment` (
   `apart_pk` int(11) NOT NULL AUTO_INCREMENT,
-  `story` varchar(45) DEFAULT NULL,
-  `floorplan` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `build_fk` int(11) DEFAULT NULL,
+  `story` int(11) DEFAULT NULL,
+  `floorplan` enum('studio','1Bed','2Bed') DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `has_internet` int(11) DEFAULT NULL,
+  `has_microwave` int(11) DEFAULT NULL,
+  `has_patio` int(11) DEFAULT NULL,
+  `has_dishwasher` int(11) DEFAULT NULL,
+  `has_washdry` int(11) DEFAULT NULL,
   PRIMARY KEY (`apart_pk`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +46,7 @@ CREATE TABLE `apartment` (
 
 LOCK TABLES `apartment` WRITE;
 /*!40000 ALTER TABLE `apartment` DISABLE KEYS */;
+INSERT INTO `apartment` VALUES (1,1,1,'studio',1,500,0,1,0,0,1),(2,2,2,'1Bed',1,700,0,1,0,1,0),(3,3,3,'2Bed',1,900,1,1,1,1,1),(4,1,1,'studio',1,550,1,0,1,0,0),(5,2,1,'studio',1,625,0,1,0,1,1),(6,1,2,'1Bed',1,720,1,0,1,1,0),(7,1,2,'2Bed',1,850,1,1,0,0,1),(8,3,2,'2Bed',1,990,1,1,1,1,0),(9,1,1,'1Bed',1,760,0,0,1,1,1),(10,1,3,'studio',1,590,0,1,0,0,0),(11,3,3,'studio',1,600,1,1,0,1,0),(12,3,1,'2Bed',1,800,1,1,1,0,1),(13,3,1,'1Bed',1,600,1,0,0,1,0),(14,1,3,'1Bed',1,730,1,0,1,0,1),(15,2,1,'2Bed',1,910,0,1,1,1,0);
 /*!40000 ALTER TABLE `apartment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,8 +62,9 @@ CREATE TABLE `building` (
   `build_name` varchar(45) DEFAULT NULL,
   `build_address` varchar(45) DEFAULT NULL,
   `build_landlord` varchar(45) DEFAULT NULL,
+  `build_city` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`build_pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +73,7 @@ CREATE TABLE `building` (
 
 LOCK TABLES `building` WRITE;
 /*!40000 ALTER TABLE `building` DISABLE KEYS */;
-INSERT INTO `building` VALUES (1,'Building1','1172. Building St.','John Doe');
+INSERT INTO `building` VALUES (1,'North','1192 N. Sunset Blvd.','Paco Archman','Gilbert'),(2,'East','9234 E. Rector St.','Benji Gentoo','Chandler'),(3,'West','246 W. Alma Ct.','Linus Torvec','Mesa');
 /*!40000 ALTER TABLE `building` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,8 +87,8 @@ DROP TABLE IF EXISTS `landlord`;
 CREATE TABLE `landlord` (
   `ll_pk` int(11) NOT NULL AUTO_INCREMENT,
   `ll_name` varchar(45) DEFAULT NULL,
-  `ll_salary` varchar(45) DEFAULT NULL,
-  `ll_gender` varchar(45) DEFAULT NULL,
+  `ll_salary` float DEFAULT NULL,
+  `ll_gender` enum('male','female') DEFAULT NULL,
   `ll_dob` datetime DEFAULT NULL,
   PRIMARY KEY (`ll_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -103,7 +112,7 @@ DROP TABLE IF EXISTS `maintenance`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `maintenance` (
   `main_pk` int(11) NOT NULL AUTO_INCREMENT,
-  `main_cost` varchar(45) DEFAULT NULL,
+  `main_cost` float DEFAULT NULL,
   `main_type` varchar(45) DEFAULT NULL,
   `main_start_date` datetime DEFAULT NULL,
   `main_end_date` datetime DEFAULT NULL,
@@ -131,7 +140,7 @@ CREATE TABLE `payment` (
   `pay_pk` int(11) NOT NULL AUTO_INCREMENT,
   `pay_type` varchar(45) DEFAULT NULL,
   `pay_date` datetime DEFAULT NULL,
-  `pay_processed` varchar(45) DEFAULT NULL,
+  `pay_processed` binary(1) DEFAULT NULL,
   PRIMARY KEY (`pay_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -155,9 +164,9 @@ DROP TABLE IF EXISTS `tenant`;
 CREATE TABLE `tenant` (
   `tena_pk` int(11) NOT NULL AUTO_INCREMENT,
   `tena_name` varchar(45) DEFAULT NULL,
-  `tena_age` varchar(45) DEFAULT NULL,
-  `tena_sex` varchar(45) DEFAULT NULL,
-  `tena_ssn` varchar(45) DEFAULT NULL,
+  `tena_age` int(11) DEFAULT NULL,
+  `tena_sex` enum('male','female') DEFAULT NULL,
+  `tena_ssn` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`tena_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -180,4 +189,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-24 14:13:30
+-- Dump completed on 2014-04-27 12:19:56
